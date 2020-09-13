edf09a737efe4e30e0d495071d0801e7d1ed717c
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2020-09-13 14:16:39 -0400

    linking both HTML and working with issues with query parameter

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,6 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
 var displayWarning = function(repo) {
     // add text to warning container
@@ -62,16 +63,31 @@ var getRepoIssues = function(repo) {
             
             // check if api has paginated issues
             if (response.headers.get("Link")) {
-                console.log("repo has more than 30 issues");
+                displayWarning(repo);
             }
           });
         }
         else {
-          alert("There was a problem with your request!");
+          // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
       });
   };
 
-
+  var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
   
-  getRepoIssues("facebook/react");
+    if (repoName) {
+      // display repo name on the page
+      repoNameEl.textContent = repoName;
+  
+      getRepoIssues(repoName);
+    } else {
+      // if no repo was given, redirect to the homepage
+      document.location.replace("./index.html");
+    }
+  };
+
+  getRepoName();
