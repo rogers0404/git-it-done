ea76d616fdec8f695e90bfeca429d82efe3dec7c
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2020-09-13 22:11:07 -0400

    handle error

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,6 +3,20 @@ var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+      if (response.ok) {
+        response.json().then(function(data) {
+          displayRepos(data.items, language);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    });
+  };
+
 var formSubmitHandler = function(event) {
     event.preventDefault();
     // get value from input element
