1bf2a0ddc07ee542afaeb68002d65aabc6d91b4f
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2020-09-13 22:31:52 -0400

    Adding buttons to search by language

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var getFeaturedRepos = function(language) {
     var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
@@ -103,4 +104,16 @@ var getUserRepos = function(user) {
 /*   getUserRepos("microsoft");
   getUserRepos("facebook"); */
 
+  var buttonClickHandler = function(event){
+      var language = event.target.getAttribute("data-language");
+      if (language) {
+        getFeaturedRepos(language);
+      
+        // clear old content
+        repoContainerEl.textContent = "";
+      }
+
+  }
+
   userFormEl.addEventListener("submit", formSubmitHandler);
+  languageButtonsEl.addEventListener("click", buttonClickHandler);
